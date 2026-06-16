c010b6d100bc21a0188047dec3be0182f4664653
sunny <anita.hr112@gmail.com>
2026-06-16 13:02:12 +0530

    In this commit i added lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,57 @@
-import React from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Index from "./Pages/Index";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import WebsiteLayout from "./component/WebsiteLayout";
-import Login from "./Pages/Login";
 import AdminLayout from "./component/Admin/AdminLayout";
-import Dashboard from "./Pages/Admin/Dashboard";
-import { Toaster } from "react-hot-toast";
 import ProtectedRoute from "./component/Admin/ProtectedRoute";
 import PublicRoute from "./component/Admin/PublicRoute";
-import Setting from "./Pages/Admin/Setting";
-import Menu from "./Pages/Admin/Menu";
+const Login = lazy(() => import("./Pages/Login"));
+const Dashboard = lazy(() => import("./Pages/Admin/Dashboard"));
+const Setting = lazy(() => import("./Pages/Admin/Setting"));
+const Menu = lazy(() => import("./Pages/Admin/Menu"));
 const App = () => {
   return (
     <div>
-      <Toaster position="top-right" />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<WebsiteLayout />}>
-            <Route path="/" element={<Index />} />
-            <Route path="*" element={<Index />} />
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
+          </div>
+        }
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route element={<WebsiteLayout />}>
+              <Route path="/" element={<Index />} />
+              <Route path="*" element={<Index />} />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute>
+                    <Login />
+                  </PublicRoute>
+                }
+              />
+            </Route>
             <Route
-              path="/login"
+              path="/dashboard"
               element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
+                <ProtectedRoute>
+                  <AdminLayout />
+                </ProtectedRoute>
               }
-            />
-          </Route>
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <AdminLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Dashboard />} />
-            <Route path="menu" element={<Menu />} />
-            <Route path="setting" element={<Setting />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            >
+              <Route index element={<Dashboard />} />
+              <Route path="menu" element={<Menu />} />
+              <Route path="setting" element={<Setting />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+      <Toaster position="top-right" />
     </div>
   );
 };
